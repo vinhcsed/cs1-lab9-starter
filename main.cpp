c909c35b6396283cef9c204034840cfeecca56af
Vincent Tran <33761211+VincentVinhTran@users.noreply.github.com>
2025-04-16 08:48:55 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
-#include <cstdlib> 
-#include <ctime>
 using namespace std;
 
-// ** REQUIRED FUNCTIONS **
-char userChoice();
-char computerChoice();
-void determineWinner(char, char);
-bool playAgain();
-// ************************
+// Menu items
+const int ADD = 1;
+const int REMOVE = 2;
+const int QUIT = 3;
+
+// Symbols
+const char EMPTY = '_';
+const char END_OF_BAG = '*';
+
+int getBagSize();
+void initBag(char*, int);
 
 int main() {
+  int bagSize = getBagSize();
+  
+  // dynamically allocate the bag array
+  char* bag;
+
+  // initialize the bag
+  initBag(bag, bagSize);
+
+  /* program logic goes here */
+
+  // deallocate the bag array
+  
   return 0;
+}
+
+int getBagSize() {
+    int sz;
+    cout << "How big is the bag?: ";
+    cin >> sz;
+    return sz;
+}
+
+void initBag(char* bag, int bagSize) {
+    bag[bagSize] = END_OF_BAG;
+    for(char* p = bag; *p != END_OF_BAG; p++) {
+        *p = EMPTY;
+    }
 }
